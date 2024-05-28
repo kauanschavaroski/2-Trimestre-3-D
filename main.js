ffb684ce50bba42937aad70119221757d3907759
kauanschavaroski <162127624+kauanschavaroski@users.noreply.github.com>
2024-05-28 10:11:10 -0300

    .

---
+++ main.js
@@ -0,0 +1,43 @@
+const caixaprincipal=document.querySelector(".caixa-principal");
+const caixaperguntas=document.querySelector(".caixa-perguntar");
+const caixaalternativas=document.querySelector(".caixa-alternativas");
+const caixaresultado=document.querySelector(".caixa-resultado");
+const textoresultado=document.querySelector(".texto-resultado");
+
+constperguntas=[
+    {
+        enunciado:"quem é mais bonito cr7 ou messi?",
+        alternativas:[
+           "cr7",
+           "messi",
+        ]
+       },
+       {
+        enunciado:"quem joga melhor cr7 ou messi?",
+        alternativas:[
+           "cr7",
+           "messi",
+        ]
+       },
+       {
+        enunciado:"quem é mais humilde?",
+        alternativas:[
+           "cr7",
+           "messi",
+        ]
+       },
+       {
+        enunciado:"quem é mais rico?",
+        alternativas:[
+           "cr7",
+           "messi",
+        ]
+       },
+       {
+        enunciado:"quem é mais famoso?",
+        alternativas:[
+           "cr7",
+           "messi",
+        ]
+       },
+    ];
